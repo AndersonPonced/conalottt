eef754ca79cef94401b8d259b45943f06fa2fa27
Anderson Ponce <nerudasho@gmail.com>
2025-03-08 20:16:16 -0400

    listo

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware para analizar el cuerpo de las solicitudes JSON
+app.use(express.json());
+
+// Middleware para analizar el cuerpo de las solicitudes urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// Endpoint POST para recibir datos de Make
+app.post('/datos-make', (req, res) => {
+  const datos = req.body;
+
+  console.log('Datos recibidos de Make:', datos);
+
+  // Envía un código de estado 204 (No Content)
+  res.status(204).send();
+});
+
+app.listen(port, () => {
+  console.log(`La API está escuchando en el puerto ${port}`);
+});
